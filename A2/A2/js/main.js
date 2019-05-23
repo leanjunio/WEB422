c066da647e3ee5300b62305b0948a0a7cf6120df
Lean Junio <leanjunio@live.com>
2019-05-23 11:54:51 -0400

    showGenericModal(title, message)

--- A2/A2/js/main.js
+++ A2/A2/js/main.js
@@ -25,9 +25,22 @@ let initializeEmployeesModel = () => {
     data.forEach(element => employeesModel.push(element));
     refreshEmployeeRows(employeesModel);
   })
-  .fail(err => {
-    // TODO: Call showGenericModal(err)
-  });
+  .fail(err => showGenericModal('Error', err));
 }
 
-
+/**
+ * Displays the genericModal with the proper contents updated.
+ * 
+ * This function does the following:
+ * - Sets the contents of *modal-title* to the passed *title* value
+ * - Sets the contents of *modal-body* to the passed *message* value
+ * - Show the genericModal using the id (genericModal)
+ * 
+ * @param {string} title 
+ * @param {string} message 
+ */
+let showGenericModal = (title, message) => {
+  $('.modal-title').val(title);
+  $('.modal-body').val(message);
+  // TODO: Show the modal
+}
