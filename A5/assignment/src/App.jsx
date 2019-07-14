0f1962f3bfada3641c34ac06c62e33062c0f711f
Lean <leanjunio@live.com>
2019-07-13 20:01:48 -0400

    add routes

--- A5/assignment/src/App.jsx
+++ A5/assignment/src/App.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 import Overview from "./components/Overview";
+import Projects from "./components/Projects";
+import Teams from "./components/Teams";
+import Employees from "./components/Employees";
+import NotFound from "./components/NotFound";
 
 class App extends Component {
   render() {
-    return <Overview />;
+    return (
+      <Switch>
+        <Route exact path="/" render={Overview} />
+        <Route path="/projects" render={Projects} />
+        <Route path="/employees" render={Employees} />
+        <Route render={NotFound} />
+      </Switch>
+    );
   }
 }
 
