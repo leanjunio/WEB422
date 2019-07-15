a12b98af7acc57ff61fa1c1007394dec17d0ee7c
Lean <leanjunio@live.com>
2019-07-14 21:39:46 -0400

    add comment

--- A5/assignment/src/App.jsx
+++ A5/assignment/src/App.jsx
@@ -1,3 +1,13 @@
+/*********************************************************************************
+* WEB422 – Assignment 04
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
+* assignment has been copied manually or electronically from any other source (including web sites) or
+* distributed to other students.
+*
+* Name: Lean Junio Student ID: 019-109-123 Date: July 14, 2019
+*
+********************************************************************************/
+
 import React, { Component } from "react";
 import { Switch, Route } from "react-router";
 import Overview from "./components/Overview";
